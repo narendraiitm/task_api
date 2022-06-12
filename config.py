b5046df163a321395ec811b69bd5b70b68f5b143
Narendra <narendra@onlinedegree.iitm.ac.in>
2022-06-12 10:13:40 +0530

    modified the uri of datatabase

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../databases/development.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///./databases/development.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
